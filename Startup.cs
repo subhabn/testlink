6211d18a1a9a9b2c29b1d3fbbe98f20e20669ca6
subhabn <subhabn@hotmail.com>
2018-04-20 13:55:16 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using BL.Link.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Services.Connectors.Link
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+             // add only the basic features of mvc like routing/formatting
+            services.AddMvcCore()
+                .AddJsonFormatters()
+                .AddApiExplorer()
+                .AddAuthorization()
+                .AddFormatterMappings()
+                .AddCacheTagHelper()
+                .AddDataAnnotations()
+                .AddCors(options =>
+                {
+                    // TODO: Review CORS policies before running in production
+                    options.AddPolicy("AllowAll",
+                        builder =>
+                        {
+                            builder
+                                .AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                                .AllowCredentials();
+                        });
+                })
+                .AddMvcOptions(options =>
+                    options.OutputFormatters.OfType<JsonOutputFormatter>().First()
+                );
+            // add DI services to be accessed dynamically here.
+            
+            var connectionString = Configuration.GetConnectionString("localDB");
+
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<DBTestContext>(options => options.UseNpgsql(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+           app.UseCors("AllowAll");
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Applications}/{action=Test}/{id?}");
+            });
+            
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Services.Connector.Link is running.");
+            });
+        }
+    }
+}
